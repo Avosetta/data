b52eb2a42cd2da4409a025815d93b91027b8d01e
Avosetta <guneet.525@gmail.com>
2024-11-24 22:45:17 -0500

    Update lab12.sql

--- lab12.sql
+++ lab12.sql
@@ -1,1 +1,41 @@
+SELECT * 
+FROM courses
+
+SELECT * FROM assignments
+LIMIT 10; 
+
+SELECT count(*) FROM courses;
+
+SELECT min(due_date) FROM assignments;
+
+SELECT *
+FROM  courses
+WHERE course_name LIKE 'Intro%';
+
+SELECT sqlite_version();
+
+SELECT upper('ABCxyz');
+
+SELECT length('abcde');
+
+SELECT 7*5;
+
+SELECT concat('ABC', '-', 'xyz');
+
+SELECT date();
+
+SELECT strftime('%Y', due_date) AS Year, * 
+FROM assignments;
+
+SELECT DISTINCT SUBSTRING(course_id, 1, 4) 
+FROM courses;
+
+SELECT SUBSTRING(course_id, 1, 4) AS prefix, count(*)
+FROM courses
+GROUP BY prefix;
+
+SELECT *
+FROM  assignments
+WHERE status != 'Completed'
+ORDER BY due_date;
 
